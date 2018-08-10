ef8ccda1c699942bb4c6a3b7c8eb6349f15b01bf
DWatson <mdwat90@gmail.com>
2018-08-10 16:43:33 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,27 @@ $(document).ready(function () {
     console.log("ready!");
 
 
+    var characterAttack;
+    var characterHealth;
+    var defenderAttack;
+    var defenderHealth;
+
+
     $("#luke").data({
         "health": 100,
-        "attack": 6
+        "attack": 6,
     })
     $("#obi-wan").data({
         "health": 120,
-        "attack": 10
+        "attack": 10,
     })
     $("#darth-s").data({
         "health": 180,
-        "attack": 15
+        "attack": 15,
     })
     $("#darth-m").data({
         "health": 150,
-        "attack": 12
+        "attack": 12,
     })
 
 
@@ -24,6 +30,7 @@ $(document).ready(function () {
     $("#attack").hide();
     $("#defenderName").hide();
     $("#newGame").hide();
+
 
     function gameStart() {
         var target = $(event.target);
@@ -57,6 +64,10 @@ $(document).ready(function () {
         }
         $("#yourCharacterName").text("Your Character");
         $("#enemiesToAttack").show();
+        characterHealth = $("#yourCharacter").find("button").data("health");
+        characterAttack = $("#yourCharacter").find("button").data("attack");
+        $("#characterHealth").text("Health: " + characterHealth).show();
+        $("#characterAttack").text("Attack Strength: " + characterAttack).show();
     }
 
 
@@ -84,16 +95,21 @@ $(document).ready(function () {
         }
         $("#attack").show();
         $("#defenderName").show();
+        defenderAttack = $("#defender").find("button").data("attack");
+        defenderHealth = $("#defender").find("button").data("health");
+        $("#defenderHealth").text("Health: " + defenderHealth).show();
+        $("#defenderAttack").text("Attack Strength: " + defenderAttack).show();
     }
 
 
-
-    $("#yourCharacter").click (function () {
+    $("#yourCharacter").click(function () {
         gameStart();
     });
 
-    $("#enemies").click (function () {
+    $("#enemies").click(function () {
         chooseDefender();
+        // turns off click ability after enemy has been chosen
+        $("#enemies").off("click");
     });
 
     // function for when attack button is clicked:
@@ -101,30 +117,54 @@ $(document).ready(function () {
     // player's health goes down by attack power of defender
     // increases player's attack power by base attack power
     $("#attack").click(function () {
-        if ($("#yourCharacter").find("button").data("health") > 0) {
-            console.log($("#yourCharacter").find("button").data("health") - $("#defender").find("button").data("attack"));
-            console.log($("#defender").find("button").data("health") - $("#yourCharacter").find("button").data("attack"));
-            console.log($("#yourCharacter").find("button").data("attack") + $("#yourCharacter").find("button").data("attack"));
+        characterHealth -= defenderAttack;
+        defenderHealth -= characterAttack;
+        characterAttack += $("#yourCharacter").find("button").data("attack");
+        $("#characterHealth").text("Health: " + characterHealth);
+        $("#characterAttack").text("Attack Strength: " + characterAttack);
+        $("#defenderHealth").text("Health: " + defenderHealth);
+        $("#defenderAttack").text("Attack Strength: " + defenderAttack);
+
+        // // If defender's value goes down to zero and player's health is > 0, defender is defeated, select new enemy
+        if (defenderHealth <= 0 && characterHealth >= 0) {
+            alert("Choose new defender!");
+            $("#defender").find("button").remove();
+            $("#defenderHealth").text("Health: " + defenderHealth).hide();
+            $("#defenderAttack").text("Attack Strength: " + defenderAttack).hide();
+            $("#enemies").click(function () {
+                chooseDefender();
+                // turns off click ability after enemy has been chosen
+                $("#enemies").off("click");
+            });
         }
-        
-        // else if all enemies defeated, player wins
-        else if ($("#enemies") === 0) {
-            alert("You Won!");
+        // If character health is 0, game over 
+        else if (characterHealth <= 0) {
+            alert("You Lose!");
             $("#newGame").show();
-        } 
-        
-        // Else player's health goes down to 0 or below, game over
-        else {
-            alert("Game Over!");
-            $("#newGame").show();
+            $("#characterHealth").text("Health: " + characterHealth).hide();
+            $("#characterAttack").text("Attack Strength: " + characterAttack).hide();
+            $("#defenderHealth").text("Health: " + defenderHealth).hide();
+            $("#defenderAttack").text("Attack Strength: " + defenderAttack).hide();
+            $("#attack").hide();
+            $("#defenderName").hide();
+            $("#enemiesToAttack").hide();
+            $("#defender").hide();
+            $("#enemies").hide();
         }
+
+        // if no more defenders in the "enemies to attack" div, you win
+        // if ($("#enemies")) {
+        //     alert("You Won!");
+        //     $("#newGame").show();
+        // }
+
+        // if no defender in the "defender" div, you can not execute attack button
+
     });
 
+    // play music automatically on page load
 
-    // If defender's value goes down to zero and player's health is > 0, defender is defeated, select new enemy
-    
-
-
-    // To Do:
-    // display "value" (health) attribute for each character
+    $("#newGame").click(function () {
+        location.reload(true);
+    });
 });
